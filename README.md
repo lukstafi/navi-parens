59239de95a39f07319b8f30dce66fdba304e58e4
lukstafi <lukstafi@gmail.com>
2022-11-14 23:13:01 +0100

    Somewhat-working up-down navigation for symbols Bug: needs double-command when at the boundary.

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Over time it might also include some other functionality and configuration that 
 ## Features
 
 Commands:
-* `goToNextBracket`: `ctrl+alt+l` Go past the next same-level closing bracket/scope
-* `goToPreviousBracket`: `ctrl+alt+j` Go to the previous same-level opening bracket/scope
-* `goToUpBracket`: `ctrl+alt+i` Go outside the opening of the current level
-* `goToDownBracket`: `ctrl+alt+k` Go outside the closing of the current level
-* `selectToNextBracket`: `shift+ctrl+alt+l` Select past the next same-level closing bracket/scope
-* `selectToPreviousBracket`: `shift+ctrl+alt+j` Select to the previous same-level opening bracket/scope
-* `selectToUpBracket`: `shift+ctrl+alt+i` Select till outside the opening of the current level
-* `selectToDownBracket`: `shift+ctrl+alt+k` Select till outside the closing of the current level
+* `goPastNextScope`: `ctrl+alt+l` Go past the next same-level closing bracket/scope
+* `goPastPreviousScope`: `ctrl+alt+j` Go to the previous same-level opening bracket/scope
+* `goToUpScope`: `ctrl+alt+i` Go outside the opening of the current level
+* `goToDownScope`: `ctrl+alt+k` Go outside the closing of the current level
+* `selectToNextScope`: `shift+ctrl+alt+l` Select past the next same-level closing bracket/scope
+* `selectToPreviousScope`: `shift+ctrl+alt+j` Select to the previous same-level opening bracket/scope
+* `selectToUpScope`: `shift+ctrl+alt+i` Select till outside the opening of the current level
+* `selectToDownScope`: `shift+ctrl+alt+k` Select till outside the closing of the current level
 
 Extra key bindings:
 * `insertCursorAtEndOfEachLineSelected`: rebound from `shift+alt+i` to `shift+alt+p`
@@ -34,7 +34,9 @@ Currently, Navi Parens uses two sources of structure information:
 
 In languages like Python these sources can be less-than-ideal. In the future, I might try to incorporate the scopes available to fold/unfold. Unfortunately, the fold/unfold scopes do not seem easily available to VSCode extensions.
 
-## TODO: remaining auto-generated suggested sections
+As an optimization, the defined-symbols are only recomputed on edit when the cursor needs to leave its current line.
+
+## TODO: provide some animations
 
 Image paths are relative to this README file.
 
@@ -43,10 +45,6 @@ For example if there is an image subfolder under your extension project workspac
 \!\[feature X\]\(images/feature-x.png\)
 
 > Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
 
 ## Extension Settings
 
@@ -63,9 +61,11 @@ This extension contributes the following settings:
 *** bind `cursorUp` to both `alt+i` and `ctrl+alt+o`.
 
 
-## Known Issues
+## Technical Details and Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Currently, multiple cursors are not supported.
+
+I ignore defined-symbols that are out-of-order with respect to the syntactic structure, e.g. Python class field definitions inside methods.
 
 ## Release Notes
 
@@ -82,26 +82,3 @@ Fixed issue #.
 ### 1.1.0
 
 Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,156 @@
 import * as vscode from 'vscode';
 
 interface DocumentNavigationState {
-  /** Cache for the symbol trees for the whole document. Lazily populated, cleared on a document change. */
-	symbols: vscode.DocumentSymbol[] | null;
-	/** A stack of ancestor symbols with the current symbol at position 0. */
-	currentSymbolZipper: vscode.DocumentSymbol[] | null;
+	/** Whether current state has potentially been invalidated, due to a document change. */
+	needsUpdate: boolean;
+
+  /** Cache for the symbol trees for the whole document. Lazily updated. */
+	rootSymbols: vscode.DocumentSymbol[];
+
+	/** A stack of ancestor symbols with the current symbol at the last (top) position. */
+	lastSymbolAndAncestors: vscode.DocumentSymbol[];
+	
+	/** The encompassing brackets scope, on last update. The scope is from the start of the opening bracket to the end of the closing bracket. */
+	lastBracketScope: vscode.Range | null;
+
+	/** Position of the cursor on last update. */
+	lastPosition: vscode.Position;
 }
 let documentStates = new Map<vscode.Uri, DocumentNavigationState>();
+
+async function updateStateForPosition(textEditor: vscode.TextEditor): Promise<DocumentNavigationState> {
+	const uri = textEditor.document.uri;
+	const pos = textEditor.selection.active;
+	let state = documentStates.get(uri);
+	if (!state) {
+		state = {needsUpdate: true, rootSymbols: [], lastSymbolAndAncestors: [], lastBracketScope: null, lastPosition: pos};
+		documentStates.set(uri, state);
+	}
+	if (state.needsUpdate) {
+		console.log('Update symbols for doc: %s -- at position %d, %d', uri, pos.line, pos.character);
+		state.rootSymbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>('vscode.executeDocumentSymbolProvider', uri);
+		state.lastSymbolAndAncestors = [];
+		state.needsUpdate = false;
+	} else if (pos.isEqual(state.lastPosition)) {
+		return state;
+	}
+	console.log('Update state for doc: %s -- at position %d, %d', uri, pos.line, pos.character);
+
+	// Locality bias: smaller position changes get processed quicker.
+	let parentSymbol;
+	while (!!(parentSymbol = state.lastSymbolAndAncestors.pop()) && !parentSymbol?.range.contains(pos)) {}	
+	let children: vscode.DocumentSymbol[] = [];
+	if (!parentSymbol || !parentSymbol.range.contains(pos)) {
+		children = state.rootSymbols;
+	} else {
+		// The last `pop` should have been `peek` (`Array` does not implement `peek` anyway).
+		state.lastSymbolAndAncestors.push(parentSymbol);
+		children = parentSymbol.children;
+	}
+	// Iteratively pick the widest child containing the position, if any.
+	while (children && children.length > 0) {
+		let candidate: vscode.DocumentSymbol | null = null;
+		for (const child of children) {
+			if (child.range.contains(pos)) {
+				if (!candidate) {
+					candidate = child;
+				} else if (child.range.contains(candidate.range)) {
+					candidate = child;
+				}
+			}
+		}
+		if (!candidate) { break; }
+		state.lastSymbolAndAncestors.push(candidate);
+		children = candidate.children;
+	}
+	state.lastPosition = pos;
+
+	// TODO: compute
+	state.lastBracketScope = null;
+
+	return state;
+}
+
+async function goToUpScope(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) {
+	let pos = textEditor.selection.active;
+	console.log('goToUpScope for doc %s -- at position: %d, %d', textEditor.document.uri, pos.line, pos.character);
+	// await printDocumentSymbols(textEditor.document);
+	let state = await updateStateForPosition(textEditor);
+	let currentSymbol = state.lastSymbolAndAncestors.pop();
+	if (currentSymbol) {
+		textEditor.selection = new vscode.Selection(currentSymbol.range.start, currentSymbol.range.start);
+	}
+}
+
+async function selectToUpScope(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) {
+	let pos = textEditor.selection.active;
+	console.log('goToUpScope for doc %s -- at position: %d, %d', textEditor.document.uri, pos.line, pos.character);
+	// await printDocumentSymbols(textEditor.document);
+	let state = await updateStateForPosition(textEditor);
+	let currentSymbol = state.lastSymbolAndAncestors.pop();
+	if (currentSymbol) {
+		textEditor.selection = new vscode.Selection(textEditor.selection.anchor, currentSymbol.range.start);
+	}
+}
+
+async function goToDownScope(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) {
+	let pos = textEditor.selection.active;
+	console.log('goToDownScope for doc %s -- at position: %d, %d', textEditor.document.uri, pos.line, pos.character);
+	// await printDocumentSymbols(textEditor.document);
+	let state = await updateStateForPosition(textEditor);
+	let currentSymbol = state.lastSymbolAndAncestors.pop();
+	if (currentSymbol) {
+		textEditor.selection = new vscode.Selection(currentSymbol.range.end, currentSymbol.range.end);
+	}
+}
+
+async function selectToDownScope(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) {
+	let pos = textEditor.selection.active;
+	console.log('goToDownScope for doc %s -- at position: %d, %d', textEditor.document.uri, pos.line, pos.character);
+	// await printDocumentSymbols(textEditor.document);
+	let state = await updateStateForPosition(textEditor);
+	let currentSymbol = state.lastSymbolAndAncestors.pop();
+	if (currentSymbol) {
+		textEditor.selection = new vscode.Selection(textEditor.selection.anchor, currentSymbol.range.end);
+	}
+}
+
+async function goPastNextScope(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) {
+	let pos = textEditor.selection.active;
+	console.log('goPastNextScope doc %s -- debug at position: %d, %d', textEditor.document.uri, pos.line, pos.character);
+	await printDocumentSymbols(textEditor.document);
+}
+
+async function goPastPreviousScope(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) {
+	let pos = textEditor.selection.active;
+	console.log('goPastNextScope doc %s -- debug at position: %d, %d', textEditor.document.uri, pos.line, pos.character);
+	await printDocumentSymbols(textEditor.document);
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "navi-parens" is being activated!');
 
+	vscode.workspace.onDidChangeTextDocument(event => {
+		const uri = event.document.uri;
+		console.log('Change in doc: %s', uri);
+		const state = documentStates.get(uri);
+		if (state) { state.needsUpdate = true; }
+	}, null, context.subscriptions);
+
 	function newCommand(command: string, callback: (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) => void) {
 		context.subscriptions.push(vscode.commands.registerTextEditorCommand(command, callback));
 	}
-	newCommand('navi-parens.goToNextBracket', goToNextBracket);
-	newCommand('navi-parens.goToPreviousBracket', goToPreviousBracket);
+
+	newCommand('navi-parens.goPastNextScope', goPastNextScope);
+	newCommand('navi-parens.goPastPreviousScope', goPastPreviousScope);
+	newCommand('navi-parens.goToUpScope', goToUpScope);
+	newCommand('navi-parens.goToDownScope', goToDownScope);
+	newCommand('navi-parens.selectToUpScope', selectToUpScope);
+	newCommand('navi-parens.selectToDownScope', selectToDownScope);
 }
 
 export function deactivate() {}
-
-async function goToNextBracket(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) {
-	let pos = textEditor.selection.active;
-	console.log('For now, debug info at position: %d, %d', pos.line, pos.character);
-	await printDocumentSymbols(textEditor.document);
-}
-
-async function goToPreviousBracket(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) {
-	let pos = textEditor.selection.active;
-	console.log('For now, debug info at position: %d, %d', pos.line, pos.character);
-	await printRangeSemanticTokens(textEditor.document, textEditor.selection);
-}
 
 async function printDocumentSymbols(doc: vscode.TextDocument) {
 	let symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>('vscode.executeDocumentSymbolProvider', doc.uri);
@@ -45,36 +164,11 @@ async function printDocumentSymbols(doc: vscode.TextDocument) {
 
 function printSymbols(symbols: vscode.DocumentSymbol[], indent: number) {	
 	for(let symbol of symbols) {
-		console.log('%s%s: %s (is string? %s)', ' '.repeat(indent), symbol.name, symbol.detail, (symbol.kind === vscode.SymbolKind.String));
-		console.log('%sChildren:', ' '.repeat(indent));
-		printSymbols(symbol.children, indent+1);
-	}
-}
-
-async function printRangeSemanticTokens(doc: vscode.TextDocument, range: vscode.Range) {
-	let cancel = new vscode.CancellationTokenSource();
-	let tokens = await vscode.commands.executeCommand<vscode.SemanticTokens>('vscode.provideDocumentRangeSemanticTokens', doc.uri, range,	cancel);
-	let legend = await vscode.commands.executeCommand<vscode.SemanticTokensLegend>('vscode.provideDocumentRangeSemanticTokensLegend', doc.uri, range,	cancel);
-	if (tokens === undefined || legend === undefined) {
-		console.log('No tokens in the range.');
-		return;
-	}
-	console.log('Range tokens:');
-	let numTokens = tokens.data.length / 5;
-	var line = 0;
-	var column = 0;
-	for (var i = 0; i < numTokens; ++i) {
-		let deltaLine = tokens.data[i*5];
-		let deltaStartChar = tokens.data[i*5+1];
-		let tokenLen = tokens.data[i*5+2];
-		let tokenType = legend.tokenTypes[tokens.data[i*5+3]];
-
-		line += deltaLine;
-		if (deltaLine == 0) {
-			column = deltaStartChar;
-		} else {
-			column += deltaStartChar;
+		console.log('%s%s: <%d,%d>--<%d,%d> %s', ' '.repeat(indent*4), symbol.name, symbol.range.start.line, symbol.range.start.character, 
+			symbol.range.end.line, symbol.range.end.character, symbol.detail);
+		if (symbol.children.length > 0) {
+			console.log('%sChildren:', ' '.repeat(indent*4));
+			printSymbols(symbol.children, indent + 1);
 		}
-		console.log('  %d,%d+%d: %s', line, column, tokenLen, tokenType);
 	}
 }
