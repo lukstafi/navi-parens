30404bc665c415c877487581fd945654f11f6cca
Lukasz Stafiniak <lukstafi@gmail.com>
2022-12-05 11:23:31 +0100

    Husky precommits don't work from the VSCode panel. Workaround is to use the terminal.

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 const testElectron1 = require("@vscode/test-electron");
 async function main() {
-    let status = null;
     try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
@@ -12,12 +11,13 @@ async function main() {
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, './suite/index');
         // Download VS Code, unzip it and run the integration test
-				console.log(`Dev: ${extensionDevelopmentPath}, test: ${extensionTestsPath}`)
-        status = await (testElectron1.runTests)({ extensionDevelopmentPath, extensionTestsPath });
+			  console.log(`Dev: ${extensionDevelopmentPath}, test: ${extensionTestsPath}`);
+        await (testElectron1.runTests)({ extensionDevelopmentPath, extensionTestsPath });
     }
     catch (err) {
       console.error('Failed to run tests');
-			console.log(`Error status ${status} -- ${err}`);
+			console.log('NOTE: For mysterious reasons, running tests on precommit does not work from ' +
+				'the VSCode source control. Use the terminal instead: `git commit -m "[explain the commit]"`.');
       process.exit(1);
     }
 }
